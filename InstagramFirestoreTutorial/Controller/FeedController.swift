06d0d54ca631b03b349da0d92446bba92e011ff0
HYUNJUNG AN <hyunzz2805@gmail.com>
2021-10-27 16:47:02 +0900

    (211027) FeedCell UI 완성
    포스트 이미지뷰, 좋아요,공유,코멘트 버튼, label 추가

--- InstagramFirestoreTutorial/Controller/FeedController.swift
+++ InstagramFirestoreTutorial/Controller/FeedController.swift
@@ -67,6 +67,12 @@ extension FeedController {
 extension FeedController: UICollectionViewDelegateFlowLayout {
     //셀 크기 조정
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 200)
+        
+        let width = view.frame.width
+        var height = width + 8 + 40 + 8
+        height += 50
+        height += 60
+        
+        return CGSize(width: width, height: height)
     }
 }
--- InstagramFirestoreTutorial/View/FeedCell.swift
+++ InstagramFirestoreTutorial/View/FeedCell.swift
@@ -14,7 +14,7 @@ class FeedCell: UICollectionViewCell {
     //2. 프로필 이미지 표시하기 위한 속성
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit //비율을 유지하면서 뷰의 사이즈에 맞게 이미지를 늘리는 옵션
+        iv.contentMode = .scaleAspectFill //비율을 유지하면서 뷰의 사이즈에 맞게 이미지를 꽉 채우는 옵션
         iv.clipsToBounds = true //내 영역(Super View) 이외 영역의 Sub View는 Draw 하지 않는다
         iv.isUserInteractionEnabled = true //이벤트가 정상적으로 view에 전달
         iv.image = #imageLiteral(resourceName: "venom-7")
@@ -30,6 +30,61 @@ class FeedCell: UICollectionViewCell {
         button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside) //버튼이벤트 함수 넣어주기
         return button
     }()
+    
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill //비율을 유지하면서 뷰의 사이즈에 맞게 이미지를 늘리는 옵션
+        iv.clipsToBounds = true //내 영역(Super View) 이외 영역의 Sub View는 Draw 하지 않는다
+        iv.isUserInteractionEnabled = true //이벤트가 정상적으로 view에 전달
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        return iv
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "like_unselected"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var commentButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "comment"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "send2"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    
+    //label 속성
+    private let likesLabel: UILabel = {
+       let label = UILabel()
+        label.text = "I like"
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Some test caption for now"
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+
+    private let postTimeLabel: UILabel = {
+       let label = UILabel()
+        label.text = "2 days ago"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
+
     
     // MARK:- Lifecycle
     
@@ -52,6 +107,24 @@ class FeedCell: UICollectionViewCell {
         //3-3.usernameButton 사용자 정의 제약조건 추가
         usernameButton.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor,
                                paddingLeft: 8)
+        
+        addSubview(postImageView)
+        postImageView.anchor(top: profileImageView.bottomAnchor, left: leftAnchor,
+                             right: rightAnchor, paddingTop: 8)
+        postImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
+        //제약을 주고싶은 아이템의 anchor 프로퍼티에 접근 -> heightAnchor
+   
+        configureActionButton()
+        
+        addSubview(likesLabel)
+        likesLabel.anchor(top: likeButton.bottomAnchor, left: leftAnchor, paddingTop: -4, paddingLeft: 8)
+        //leftAnchor는 절대적인 위치
+        
+        addSubview(captionLabel)
+        captionLabel.anchor(top: likesLabel.bottomAnchor, left: leftAnchor, paddingTop: 8,  paddingLeft: 8)
+        
+        addSubview(postTimeLabel)
+        postTimeLabel.anchor(top: captionLabel.bottomAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 8)
     }
       
     required init?(coder: NSCoder) {
@@ -63,6 +136,20 @@ class FeedCell: UICollectionViewCell {
     //3-1. 버튼 액션 메소드 생성
     @objc func didTapUsername() {
         print("DEBUG: didTapUsername") //버튼마다 프린트 해주기!
+    }
+    
+    //MARK:- Helpers
+
+    func configureActionButton() {
+        //StackView란 Auto Layout을 이용해 열 또는 행에 View들의 묶음을 배치할 수 있는 간소화된 인터페이스
+        let stackView = UIStackView(arrangedSubviews: [likeButton,commentButton,shareButton])
+        //arrangedSubviews 배열의 순서에 따라 StackView의 축(axis)을 따라 배치
+        stackView.axis = .horizontal //가로 방향 축
+        stackView.distribution = .fillEqually
+        //fillEqually 플래그는 StackView의 Axis(축)에 따라 StackView를 채우기 위해 모든 Arrange Views들의 사이즈를 동일하게 재조정
+        
+        addSubview(stackView)
+        stackView.anchor(top: postImageView.bottomAnchor, width: 120, height: 50) //위치 지정
     }
     
 }
